2c59119e61af6f11b97fd3df4fff91a7e8d8711c
IsabelCab <isabel.cabral89@myhunter.cuny.edu>
2026-04-12 21:00:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.addEventListener("mousemove", function(e) {
+  const sparkle = document.createElement("div");
+  sparkle.classList.add("sparkle");
 
+  sparkle.style.left = e.pageX + "px";
+  sparkle.style.top = e.pageY + "px";
+
+  document.body.appendChild(sparkle);
+
+  setTimeout(() => {
+    sparkle.remove();
+  }, 800);
+});
